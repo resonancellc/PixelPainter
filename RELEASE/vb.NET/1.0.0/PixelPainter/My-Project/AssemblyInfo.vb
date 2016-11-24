f502f8563775338c5c42ae3d276bcd3ee4a4e28c
Nanomotion <Nanomotion@users.noreply.github.com>
2016-11-23 20:13:02 -0500

    Add files via upload

---
+++ RELEASE/vb.NET/1.0.0/PixelPainter/My-Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PixelPainter")> 
+<Assembly: AssemblyDescription("An application that lets you paint pixels on a grid")> 
+<Assembly: AssemblyCompany("Nanomotion")> 
+<Assembly: AssemblyProduct("PixelPainter")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016 Nanomotion")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3b2469ad-d816-4cd1-9ea8-44a9e0446d72")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en-US")> 
